a8933e6acc7bb0bb62c83d7e77a643c7109756d3
Anthony S Liantonio <csasl@wakefern.com>
2017-08-16 16:39:53 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { StatusComponent } from './status/status.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { MdSidenavModule, MdToolbarModule, MdTabsModule, MdButtonModule } from '@angular/material';
+
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: StatusComponent
+  }
+  // {
+  //   path: '',
+  //   redirectTo: '/status',
+  //   pathMatch: 'full'
+  // }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    StatusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {useHash:true}),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdTabsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
