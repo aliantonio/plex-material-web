7933fb1d8116e7549058099538d0b1a0b18d105e
Anthony Liantonio <anthony.liantonio@gmail.com>
2018-02-05 20:09:51 -0500

    join api

--- src/app/join-api.service.ts
+++ src/app/join-api.service.ts
@@ -8,7 +8,7 @@ export class JoinAPIService {
   constructor(private http: HttpClient) { }
 
   push(param: string, email?: any) {
-    let deviceId = '3ef962b71fa546a3868bbc4dd3d24370'; // pixel 2
+    let deviceId = '18353c5d51b74659a7a7e38d93449da4'; // pixel 2
     let apiKey = '36daccd47ff14aa385a36d425ab4bc13';
     console.log('https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?text=' + param + email + '&deviceId=' + deviceId + '&apikey=' + apiKey);
     this.http.get('https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?text=' + param + email + '&deviceId=' + deviceId + '&apikey=' + apiKey)
