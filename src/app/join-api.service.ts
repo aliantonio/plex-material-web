5b8419f72db5500bce9c2fcd51811412ffc851ed
Anthony S Liantonio <csasl@wakefern.com>
2018-05-01 10:23:40 -0400

    new join device id

--- src/app/join-api.service.ts
+++ src/app/join-api.service.ts
@@ -8,7 +8,7 @@ export class JoinAPIService {
   constructor(private http: HttpClient) { }
 
   push(param: string, email?: any) {
-    let deviceId = '18353c5d51b74659a7a7e38d93449da4'; // pixel 2
+    let deviceId = '9a4d56c7d6ed4580baaa019acecbe5a1'; // pixel 2
     let apiKey = '36daccd47ff14aa385a36d425ab4bc13';
     console.log('https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?text=' + param + email + '&deviceId=' + deviceId + '&apikey=' + apiKey);
     this.http.get('https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?text=' + param + email + '&deviceId=' + deviceId + '&apikey=' + apiKey)
